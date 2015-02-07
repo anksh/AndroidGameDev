74e905b6230745f0881da84391c3b7883c109768
Ankit Shah <ank.h.shah@gmail.com>
2015-02-07 17:57:32 -0500

    First Commit

--- app/src/main/java/com/omkarmoghe/androidgamedev/MainActivity.java
+++ app/src/main/java/com/omkarmoghe/androidgamedev/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,7 +14,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();// Hides the action bar
-        setContentView(new GameView(this));
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button_id);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                setContentView(new GameView(MainActivity.this));
+            }
+        });
     }
 
     @Override
